90e00889e8eba854d6aaafcd0f28e772ac885ac2
Oleg Brazhnichenko <oleg.brazhnichenko@gmail.com>
2017-07-18 17:36:48 +0300

    sort add

--- pset2/task11/script.js
+++ pset2/task11/script.js
@@ -2,6 +2,7 @@ function makeLinks(){
     var links = document.getElementById('links').value;
     links = links.replace(/http:\/\/|https:\/\//gi,'');
     links = links.split(',');
+    links = links.sort();
     document.getElementById('result').innerHTML = '';
     for(var i = 0; i < links.length; i++){
         document.getElementById('result').innerHTML += '<li><a href="'+links[i]+'">'+links[i]+'</a></li>'
