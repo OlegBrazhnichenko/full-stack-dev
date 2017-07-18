2adebd731fe7a61638bac83747620f2dcaceb6e1
Oleg Brazhnichenko <oleg.brazhnichenko@gmail.com>
2017-07-18 17:31:22 +0300

    but fix

--- pset2/task11/script.js
+++ pset2/task11/script.js
@@ -2,7 +2,8 @@ function makeLinks(){
     var links = document.getElementById('links').value;
     links = links.replace(/http:\/\/|https:\/\//gi,'');
     links = links.split(',');
+    document.getElementById('result').innerHTML = '';
     for(var i = 0; i < links.length; i++){
-        document.getElementById('result').innerHTML = '<li><a href="'+links[i]+'">'+links[i]+'</a></li>'
+        document.getElementById('result').innerHTML += '<li><a href="'+links[i]+'">'+links[i]+'</a></li>'
     }
 }
