7047a89e51ab44bae7c5c69b19fb509556ad0199
Oleg Brazhnichenko <oleg.brazhnichenko@gmail.com>
2017-09-05 18:56:44 +0300

    delete duplicated jquery selectors

--- pset3/customSelect/customSelect.js
+++ pset3/customSelect/customSelect.js
@@ -11,12 +11,12 @@ function handleAllSelects(){
 
 function handleSelect(select, selectOptions){
     selectOptions = selectOptions || [{data : []}]; // if no data, set default empty data
-
-    if(!$(select).find('.selectedOption').length){
-        $(select).append("<div class='selectedOption'><span>"+$(select).attr("data-placeholder")+"</span></div>");
-        $(select).append("<div class='options'></div>");
-        $(select).find(".selectedOption").on("click", function(){
-            $(select).find(".options").toggle();
+    select = $(select);
+    if(!select.find('.selectedOption').length){
+        select.append("<div class='selectedOption'><span>"+$(select).attr("data-placeholder")+"</span></div>");
+        select.append("<div class='options'></div>");
+        select.find(".selectedOption").on("click", function(){
+            select.find(".options").toggle();
         });
     }
 
@@ -24,32 +24,32 @@ function handleSelect(select, selectOptions){
 }
 
 function addOptions(select, options){
-    console.log(options);
     select = $("#"+select);
     for (var i = 0; i < options.length; i++){
         var option = document.createElement('div');
         option.className = 'customOption';
         option.setAttribute('data-img', options[i].img);
         option.setAttribute('data-value', options[i].value);
-        $(select).append(option);
+        select.append(option);
     }
 
     handleSelect(select[0], getOptionsFromSelect(select[0]));
 }
 
 function handleOptions(select, selectOptions){
-
-    var optionElements = $(select).find("div.customOption");
-    $(select).find("div.options").append(optionElements);
+    select = $(select);
+    var optionElements = select.find("div.customOption");
+    select.find("div.options").append(optionElements);
     for ( var i = 0; i < selectOptions.data.length; i++ ) {
-        $(optionElements[i]).append(
+        var optionElem = $(optionElements[i]);
+            optionElem.append(
             "<img src='"+selectOptions.data[i].img+"' />" +
             "<span>"+selectOptions.data[i].value+"</span>"
         );
-        $(optionElements[i]).on("click", function(e){
-            $(select).find("div.selected").toggleClass("selected");
+        optionElem.on("click", function(e){
+            select.find("div.selected").toggleClass("selected");
             $(this).toggleClass("selected");
-            $(select).find(".options").toggle();
+            select.find(".options").toggle();
             handleSelectedOptions(select);
         });
     }
@@ -63,9 +63,10 @@ function getOptionsFromSelect(select){
         data : []
     };
     for (var i = 0; i < optionElements.length; i++) {
+        var optionElement = $(optionElements[i]);
         options.data.push({
-            value: $(optionElements[i]).attr("data-value"),
-            img : $(optionElements[i]).attr("data-img")
+            value: optionElement.attr("data-value"),
+            img : optionElement.attr("data-img")
         })
     }
 
@@ -73,13 +74,14 @@ function getOptionsFromSelect(select){
 }
 
 function handleSelectedOptions(select){
-    var selectedOption = $(select).find("div.selected");
+    select = $(select);
+    var selectedOption = select.find("div.selected");
     if (selectedOption.length > 1) {
         console.error("There should be only one selected option.");
 
         return false;
     }
     selectedOption = selectedOption[0];
-    $(select).find("div.selectedOption")[0].innerHTML = selectedOption.innerHTML;
-    $(select).trigger("change"); // fire event for user to have an ability to handle change.
+    select.find("div.selectedOption")[0].innerHTML = selectedOption.innerHTML;
+    select.trigger("change"); // fire event for user to have an ability to handle change.
 }
