5a546f2eb82bc558d7b4c06618c9c127d295f2df
Oleg Brazhnichenko <oleg.brazhnichenko@gmail.com>
2017-09-21 14:39:45 +0300

    implement of responsive design

---
+++ practice/perth/gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var sass = require('gulp-sass');
+
+gulp.task('watch_scss', function(){
+    gulp.src('sass/**/*.scss')
+        .pipe(watch('sass/**/*.scss'))
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('css'));
+});
+
+gulp.task('watch', ['watch_scss']);
+
+gulp.task('default', ['watch']);
