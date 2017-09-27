2b72f99d8b08ead34cbec5344f7bd84ee07abbcd
Oleg Brazhnichenko <oleg.brazhnichenko@gmail.com>
2017-09-27 17:14:18 +0300

    request handle change

--- pset4/task7/server.php
+++ pset4/task7/server.php
@@ -1,6 +1,7 @@
 <?php
+
     $votes = json_decode(file_get_contents("./votes.json"), true);
-    $data = json_decode(file_get_contents('php://input'), true);
-    $votes[$data['vote']]++;
+    $data = $_POST["vote"];
+    $votes[$data]++;
     file_put_contents('./votes.json', json_encode($votes));
     print_r(json_encode($votes));
